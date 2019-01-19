990b249b6259c632746bcdc0c2f6cdc9c3e3f79c
Art G <theartofnoise@gmail.com>
2019-01-19 11:15:15 -0500

    database

--- logic.js
+++ logic.js
@@ -1,3 +1,18 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAS0mQChNLM2WUtM8RpriXMxKahCMkdtTw",
+    authDomain: "employee-data-managment-84984.firebaseapp.com",
+    databaseURL: "https://employee-data-managment-84984.firebaseio.com",
+    projectId: "employee-data-managment-84984",
+    storageBucket: "employee-data-managment-84984.appspot.com",
+    messagingSenderId: "194894590693"
+  };
+  firebase.initializeApp(config);
+
+  var db = firebase.database().ref();
+
+
 var newRow = $("<tr>");
 var tHead = $("<th>");
 var newData = $("<td>");
@@ -16,9 +31,10 @@ var newEmp = {
 
 
 
+
 $('#add').on('click', function () {
-    $('#topRow').append(newRow) 
-    $('#topRow').appendchild(tHead) 
-    $('#topRow').appendchild(newData) 
+    $('#topRow').append(newRow) ;
+    $('#topRow').append(tHead) ;
+    $('#topRow').append(newData); 
 });
 
