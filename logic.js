b94f06d0fbb70d44417d37804415f63c64873238
enricb94 <enricbaduell@me.com>
2019-01-19 10:59:25 -0500

    added JS

---
+++ logic.js
@@ -0,0 +1,10 @@
+var newRow = $("<tr>");
+var tHead = $("<th>");
+var newData = $("<td>");
+
+
+$('#add').on('click', function () {
+    $('#topRow').append(newRow) 
+    $('#topRow').appendchild(tHead) 
+    $('#topRow').appendchild(newData) 
+});
