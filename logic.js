06903c6ef3a6b541714b26665dd92c91cd683400
Art G <theartofnoise@gmail.com>
2019-01-19 11:08:31 -0500

    vars added

--- logic.js
+++ logic.js
@@ -2,9 +2,23 @@ var newRow = $("<tr>");
 var tHead = $("<th>");
 var newData = $("<td>");
 
+var empName = $("#nameIn");
+var empRole = $("#roleIn");
+var empStart = $("#startIn");
+var empRate = $("#rateIn");
+
+var newEmp = {
+    name:empName,
+    role:empRole,
+    start:empStart,
+    rate:empRate
+}
+
+
 
 $('#add').on('click', function () {
     $('#topRow').append(newRow) 
     $('#topRow').appendchild(tHead) 
     $('#topRow').appendchild(newData) 
 });
+
